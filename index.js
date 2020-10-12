7747364483d553d452c849c4e6ec3a6cacfedbe4
Gabriel Slama <gabriel.slama@akqa.com>
2020-10-12 11:07:11 +0200

    Add react file and deps

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require("./eslintrc.json");
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: "babel-eslint",
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "latest",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["./"],
+      },
+    },
+  },
+};
